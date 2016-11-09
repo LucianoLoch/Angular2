92d2d8df027116f91a2b12cb39aa3a3fae5f95a6
Luciano.Loch <luciano.loch@senior.com.br>
2016-11-09 13:15:02 -0200

    08 - Forms - TD - ngModel Two Way Databinding

--- forms/src/app/template-driven/template-driven.component.ts
+++ forms/src/app/template-driven/template-driven.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
 
+import { NgForm } from '@angular/forms';
+
 @Component({
   selector: 'template-driven',
   templateUrl: 'template-driven.component.html'
 })
 export class TemplateDrivenComponent {
+
+  user = {
+    username: 'Luciano',
+    email: 'luciano@teste.com',
+    password: 'teste'
+  };
+
+  onSubmit(form: NgForm){
+    console.log(this.user);
+  }
 }
